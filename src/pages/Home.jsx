7a561471e6b5a7b7427a9bb91b8d4b829a238047
Danielle <daniell07720@gmail.com>
2025-05-27 23:40:59 +0800

    feat: adding animation in text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaSteam,
+} from "react-icons/fa";
 import CatImage from "@/assets/images/Cat.png";
-import ResumePdf from "@/assets/documents/resume.pdf";
 import AnimatedText from "@/components/AnimatedText";
 
 const Home = () => {
@@ -17,27 +23,45 @@ const Home = () => {
             style={{ animationDelay: "0.2s" }}
           />
           <h1
-            className="text-4xl md:text-4xl font-extrabold mb-4 animate-fadeUp"
+            className="text-4xl md:text-4xl font-extrabold mb-4 animate-fadeUp font-mono"
             style={{ animationDelay: "0.4s", animationFillMode: "forwards" }}
           >
             Hi, I'm{" "}
             <span className="text-indigo-500">
-              <AnimatedText texts={["Danielle Ledesma Lunas", "Casual Dota Player"]} />
+              <AnimatedText
+                texts={["Danielle Ledesma Lunas", "Casual Dota Player"]}
+              />
             </span>
           </h1>
           <p
-            className="text-lg md:text-xl text-gray-300 mb-6 animate-fadeUp"
+            className="text-lg md:text-xl text-gray-300 mb-6 animate-fadeUp font-mono"
             style={{ animationDelay: "0.6s", animationFillMode: "forwards" }}
           >
-            I'm a passionate{" "}
-            <span className="text-indigo-400">Full-Stack Developer</span>
-            <br />
-            who builds beautiful and functional web applications.
+            Aspiring{" "}
+            <span className="text-indigo-400">
+              Back-End Developer | IT Support Specialist
+            </span>
           </p>
           <div
-            className="flex justify-center space-x-6 mb-8 animate-fadeUp"
+            className="flex justify-center space-x-6 mb-8 animate-fadeUp font-mono"
             style={{ animationDelay: "0.8s", animationFillMode: "forwards" }}
           >
+            <a
+              href="https://www.facebook.com/mnemosynedan.06"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-indigo-400 transition"
+            >
+              <FaFacebook size={28} />
+            </a>
+            <a
+              href="https://www.instagram.com/miracl3_dreams/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-indigo-400 transition"
+            >
+              <FaInstagram size={28} />
+            </a>
             <a
               href="https://github.com/miracl3-dreams"
               target="_blank"
@@ -60,15 +84,15 @@ const Home = () => {
             >
               <FaEnvelope size={28} />
             </a>
+            <a
+              href="https://steamcommunity.com/id/mnemosynedan06/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-indigo-400 transition"
+            >
+              <FaSteam size={28} />
+            </a>
           </div>
-          <a
-            href={ResumePdf}
-            download="Danielle_Lunas_Resume.pdf"
-            className="inline-block bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-3 px-6 rounded-full transition animate-fadeUp"
-            style={{ animationDelay: "1s", animationFillMode: "forwards" }}
-          >
-            Download Resume
-          </a>
         </div>
       </section>
     </>
