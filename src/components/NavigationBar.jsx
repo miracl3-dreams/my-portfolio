14a91834f4f5e16c62093d72de7ccfb7bf7737cb
Danielle <daniell07720@gmail.com>
2025-06-09 02:55:22 +0800

    fixed: changing link to a href

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross2 } from "react-icons/rx";
 import DarkMode from "../components/Darkmode";
@@ -19,16 +18,19 @@ const NavigationBar = () => {
     <>
       <ul className="hidden lg:flex fixed top-5 left-1/2 -translate-x-1/2 bg-indigo-400 dark:bg-black/30 backdrop-blur-lg px-10 py-3 rounded-full shadow-lg text-black dark:text-white items-center gap-8 font-bold z-50 font-mono">
         <li className="hover:text-black dark:hover:text-black hover:underline underline-offset-4">
-          <Link to="/home">Home</Link>
+          <a href="#home">Home</a>
         </li>
         <li className="hover:text-black dark:hover:text-black hover:underline underline-offset-4">
-          <Link to="/about">About</Link>
+          <a href="#about">About</a>
         </li>
         <li className="hover:text-black dark:hover:text-black hover:underline underline-offset-4">
-          <Link to="/skills">Skills</Link>
+          <a href="#skills">Skills</a>
         </li>
         <li className="hover:text-black dark:hover:text-black hover:underline underline-offset-4">
-          <Link to="/projects">Projects</Link>
+          <a href="#projects">Projects</a>
+        </li>
+        <li className="hover:text-black dark:hover:text-black hover:underline underline-offset-4">
+          <a href="#contacts">Contacts</a>
         </li>
         <li>
           <DarkMode />
@@ -46,19 +48,21 @@ const NavigationBar = () => {
 
       {mobileView && (
         <div className="lg:hidden fixed inset-0 z-50 bg-black text-white flex flex-col items-center justify-center gap-8 font-bold font-mono">
-          <Link to="/home" onClick={closeMobileMenu}>
+          <a href="#home" onClick={closeMobileMenu}>
             Home
-          </Link>
-          <Link to="/about" onClick={closeMobileMenu}>
+          </a>
+          <a href="#about" onClick={closeMobileMenu}>
             About
-          </Link>
-          <Link to="/skills" onClick={closeMobileMenu}>
+          </a>
+          <a href="#skills" onClick={closeMobileMenu}>
             Skills
-          </Link>
-          <Link to="/projects" onClick={closeMobileMenu}>
+          </a>
+          <a href="#projects" onClick={closeMobileMenu}>
             Projects
-          </Link>
-
+          </a>
+          <a href="#contacts" onClick={closeMobileMenu}>
+            Contacts
+          </a>
           <DarkMode />
         </div>
       )}
