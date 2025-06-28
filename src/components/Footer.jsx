f145af605b36e504bd368fc99e259267c07cf19d
Danielle <daniell07720@gmail.com>
2025-06-28 14:33:37 +0800

    feat: added bg color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="bg-blue-900 text-white py-6 px-4">
-        <div className="text-center text-sm text-gray-300 dark:text-gray-400 font-mono">
+      <footer className="bg-indigo-400 text-white py-6 px-4">
+        <div className="text-center text-sm text-black dark:text-black font-mono">
           &copy; {new Date().getFullYear()} Danielle Lunas. All rights reserved.
         </div>
       </footer>
